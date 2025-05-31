27fe124af14da7146972818fe98e41d19e1a4dc0
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-05-31 13:59:09 -0500

    Pushing new bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+python3 -m pip install simple_http_server
+sudo yum install git -y
+git clone https://github.com/tkerbe2/tkdev-terraform-web-app
+cd tkdev-terraform-web-app
+sudo chmod 700 bash-html-create.sh
+./bash-html-create.sh
+sudo python3 -m http.server 80 >> output.html
