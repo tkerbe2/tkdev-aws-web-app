a473793314ed0c5e942f7ff6492215cee1e5f987
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-05-31 13:53:28 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,46 @@
+#               _               _    _    __ 
+#              | |             | |  | |  / _|
+#    ___  _   _| |_ _ __  _   _| |_ | |_| |_ 
+#   / _ \| | | | __| '_ \| | | | __|| __|  _|
+#  | (_) | |_| | |_| |_) | |_| | |_ | |_| |  
+#   \___/ \__,_|\__| .__/ \__,_|\__(_)__|_|  
+#                  | |                       
+#                  |_|                       
+
+#=======================#
+# Outputs for resources #
+#=======================#
+
+output "vpc_main_out" {
+    value = aws_vpc.main_vpc.id
+}
+
+output "igw_id_out" {
+    value = aws_internet_gateway.main_igw.id
+}
+
+output "elb_bucket_out" {
+    value = aws_s3_bucket.lb_logs_bucket.id
+}
+
+output "alb_out" {
+    value = aws_lb.alb.id
+}
+
+output "web_server_out" {
+for_each = local.availability_zones
+
+    value = aws_instance.web_server[each.key].id
+}
+
+output "web_server_out" {
+for_each = local.availability_zones
+
+    value = aws_instance.web_server[each.key].id
+}
+
+output "web_server_eni_out" {
+for_each = local.availability_zones
+
+    value = aws_network_interface.web_server_eni[each.key].id
+}
