8a37aa286878cc89a8114a656ddf0fe5068d8bff
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-05-30 22:53:00 -0500

    Update bash-html-create.sh
    tested and confirmed this code works.

--- bash-html-create.sh
+++ bash-html-create.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+# These echo commands will create a basic website in HTML with a unique public IP
+# This will help us identify the success of the load balancing
+
+# I use escape characters in the echo command to print basic HTML code line-by-line
 echo \<\!DOCTYPE html\> > landing.html
 echo \<html\> >> landing.html
 echo \<head\> >> landing.html
 echo \<title\>web server\<\/title\> >> landing.html
-echo \</head\> >> landing.html
+echo \<\/head\> >> landing.html
 echo \<body\> >> landing.html
-echo \<h1\>This is a web server\:\<\/h1\>" >> landing.html
+echo \<h1\>This is a web server\:\<\/h1\> >> landing.html
 echo \<h1\>
+# This curl command uses a website dedicated to showing servers their public IPs and returns a simple value
 curl ifconfig.me >> landing.html
 echo \<\/h1\>
-echo "\<\/body\>" >> landing.html
-echo "\<\/\html\>" >> landing.html
+echo \<\/body\> >> landing.html
+echo \<\/\html\> >> landing.html
+
