ddd715737cbbf2bf324ccf5169088399d098eea4
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-05-31 13:55:04 -0500

    Create route.tf

---
+++ route.tf
@@ -0,0 +1,26 @@
+#                   _        _    __ 
+#                  | |      | |  / _|
+#   _ __ ___  _   _| |_ ___ | |_| |_ 
+#  | '__/ _ \| | | | __/ _ \| __|  _|
+#  | | | (_) | |_| | ||  __/| |_| |  
+#  |_|  \___/ \__,_|\__\___(_)__|_|  
+                                   
+
+#=====================#
+# Default Route Table #
+#=====================#
+resource "aws_default_route_table" "default_rt" {
+    vpc_id = aws_vpc.main_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main_igw.id
+    }
+    
+
+    tags = {
+    Name = "${local.name_prefix}_main_rt"
+  }
+  
+  depends_on = [aws_subnet.web_sn]
+}
