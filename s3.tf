8379c31987e5bc647e324b702937c073eeec1398
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-05-31 13:55:16 -0500

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,35 @@
+#       ____  _    __ 
+#      |___ \| |  / _|
+#   ___  __) | |_| |_ 
+#  / __||__ <| __|  _|
+#  \__ \___) | |_| |  
+#  |___/____(_)__|_|  
+
+
+#===============#
+# Random String #
+#===============#
+
+# An example of using random string to generate some random numbers to fill our bucket name
+# AWS S3 bucket names have to be globally unique so this is an easy way to accomplish this
+resource "random_string" "random_bucket_name" {
+  length = 10
+  special = false
+  lower = false
+  upper = false
+  numeric = true
+
+}
+#===========#
+# S3 Bucket #
+#===========#
+
+# This creates an S3 bucket with our random string
+resource "aws_s3_bucket" "lb_logs_bucket" {
+  bucket = "${local.name_prefix}_${random_string.random_bucket_name.result}_bucket"
+
+  tags = {
+    Name        = "${local.name_prefix}_${random_string.random_bucket_name.result}_bucket"
+    Environment = var.env
+  }
+}
