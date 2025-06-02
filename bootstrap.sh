bcea0007f74cf75b510377929ad151f5b5219e4c
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-06-01 20:07:11 -0500

    changing script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo yum install python3 -y
-sudo yum install python3-pip -y
-sudo python3 -m pip install simple_http_server -y
-sudo yum install git -y
+yum install python3 -y
+yum install python3-pip -y
+python3 -m pip install simple_http_server -y
+yum install git -y
 git clone https://github.com/tkerbe2/tkdev-terraform-web-app
 cd tkdev-terraform-web-app
-sudo chmod 700 bash-html-create.sh
+chmod 700 bash-html-create.sh
 ./bash-html-create.sh
-sudo python3 -m http.server 80 >> output.html
+python3 -m http.server 80 >> output.html
