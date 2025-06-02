b6e9ca490bb4368e98b2a738b72cf73c43571f02
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-06-01 20:02:52 -0500

    Update bash-html-create.sh

--- bash-html-create.sh
+++ bash-html-create.sh
@@ -6,9 +6,9 @@ echo \<head\> >> landing.html
 echo \<title\>web server\<\/title\> >> landing.html
 echo \</head\> >> landing.html
 echo \<body\> >> landing.html
-echo \<h1\>This is a web server\:\<\/h1\>" >> landing.html
+echo \<h1\>This is a web server\:\<\/h1\> >> landing.html
 echo \<h1\>
 curl ifconfig.me >> landing.html
 echo \<\/h1\>
-echo "\<\/body\>" >> landing.html
-echo "\<\/\html\>" >> landing.html
+echo \<\/body\> >> landing.html
+echo \<\/\html\> >> landing.html
