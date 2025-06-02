8dd52d451f57a4d8188cfb85fb0df952074dacc8
Taylor <69871115+tkerbe2@users.noreply.github.com>
2025-06-01 20:05:54 -0500

    test server

--- bash-html-create.sh
+++ bash-html-create.sh
@@ -7,8 +7,8 @@ echo \<title\>web server\<\/title\> >> landing.html
 echo \</head\> >> landing.html
 echo \<body\> >> landing.html
 echo \<h1\>This is a web server\:\<\/h1\> >> landing.html
-echo \<h1\>
+echo \<h1\> >> landing.html
 curl ifconfig.me >> landing.html
-echo \<\/h1\>
+echo \<\/h1\> >> landing.html
 echo \<\/body\> >> landing.html
 echo \<\/\html\> >> landing.html
